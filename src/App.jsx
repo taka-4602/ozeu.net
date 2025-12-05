a163809b8e0a306968a74f1929c077648d14a089
hirorogo <hiro114514@proton.me>
2025-12-05 18:10:36 +0900

    アプリのスタイルシートを削除し、ページコンポーネントを追加。基本的なレイアウトを整備。

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to </h1>
+      <p>Your one-stop solution for all things related to </p>
+      
+    </div>
+  )
+}
