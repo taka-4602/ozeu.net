7b11077b5b62dad0aeeaead1a455fab85c1e8ffa
hirorogo <hiro114514@proton.me>
2025-12-05 18:15:29 +0900

    ページコンポーネントを追加し、不要なコンポーネントとスタイルシートを削除。アプリの構造を整理。

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Home from "./pages/Home/Home";
+import Tools from "./pages/Tools/Tools";
+import Gallery from "./pages/Gallery/Gallery";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 
 export default function App() {
   return (
