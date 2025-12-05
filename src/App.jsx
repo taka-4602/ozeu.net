99e268c92f5d4593afb393d81f921b9f98610e8d
hirorogo <hiro114514@proton.me>
2025-12-05 17:30:46 +0900

    かきくけ

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,27 @@ import Gallery from "./pages/Gallery";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
+import React from 'react'
+export default function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/tools">Tools</Link></li>
+          <li><Link to="/gallery">Gallery</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tools" element={<Tools />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  )
+}
